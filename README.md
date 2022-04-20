0f6a2f961ff6a05c380e29d6c9e1b691089e6737
Dan Visel <dbvisel@gmail.com>
2022-04-20 17:38:37 +0800

    fonts, prettier

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aftertheendofthe
+
+## TODO:
+
+ - figure out a smart way to do markdown
+ - images
+ - fonts/styling
---
+++ components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Made by <a href="https://withhiddennoise.net">Dan Visel</a>, 2022.
+    </footer>
+  );
+}
