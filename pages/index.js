8424d281bee026b7ef3e8b7ec6c6ca260bc25e1c
Dan Visel <dbvisel@gmail.com>
2022-04-20 17:08:21 +0800

    initial update

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>After the end of the</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="After the end of the" />
+        <p className="description">soon.</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
